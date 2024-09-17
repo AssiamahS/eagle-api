2380176bcd0d06c9d6f3ab4593c96d1a34ae44e8
SylvesterAssiamah <122726691+AssiamahS@users.noreply.github.com>
2024-09-17 12:02:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,136 +1,54 @@
 "use strict";
 
-var app           = require("express")();
-var fs            = require('fs');
-var uploadDir     = process.env.UPLOAD_DIRECTORY || "./uploads/";
-var hostname      = process.env.API_HOSTNAME || "localhost:3000";
-var swaggerTools  = require("swagger-tools");
-var YAML          = require("yamljs");
-var mongoose      = require("mongoose");
-var passport      = require("passport");
-var auth          = require("./api/helpers/auth");
-var models        = require("./api/helpers/models");
-var swaggerConfig = YAML.load("./api/swagger/swagger.yaml");
-var winston       = require('winston');
-var bodyParser    = require('body-parser');
+import express from 'express';
+import fs from 'fs';
+import swaggerTools from 'swagger-tools';
+import YAML from 'yamljs';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import winston from 'winston';
+import bodyParser from 'body-parser';
+import auth from './api/helpers/auth';
+import models from './api/helpers/models';
 
-var dbConnection  = 'mongodb://'
-                    + (process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost')
-                    + '/'
-                    + (process.env.MONGODB_DATABASE || 'epic');
-var db_username = process.env.MONGODB_USERNAME || '';
-var db_password = process.env.MONGODB_PASSWORD || '';
+const app = express();
+const uploadDir = process.env.UPLOAD_DIRECTORY || "./uploads/";
+const hostname = process.env.API_HOSTNAME || "localhost:3000";
+const swaggerConfig = YAML.load("./api/swagger/swagger.yaml");
+
+const dbConnection = `mongodb://${process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost'}/${process.env.MONGODB_DATABASE || 'epic'}`;
+const db_username = process.env.MONGODB_USERNAME || '';
+const db_password = process.env.MONGODB_PASSWORD || '';
 
 // Logging middleware
-winston.loggers.add('default', {
-    console: {
-        colorize: 'true',
-        handleExceptions: true,
-        json: false,
-        level: 'silly',
-        label: 'default',
-    }
+const defaultLog = winston.createLogger({
+    level: 'silly',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.label({ label: 'default' }),
+        winston.format.simple()
+    ),
+    transports: [
+        new winston.transports.Console({
+            handleExceptions: true
+        })
+    ]
 });
-var defaultLog = winston.loggers.get('default');
 
-// Increase postbody sizing
-app.use(bodyParser.json({limit: '10mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
+// Increase post body sizing
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 // Enable CORS
-app.use(function (req, res, next) {
-  defaultLog.info(req.method, req.url);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,responseType');
-  res.setHeader('Access-Control-Expose-Headers', 'x-total-count,x-pending-comment-count,x-next-comment-id');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Cache-Control', 'max-age=4');
-  next();
+app.use((req, res, next) => {
+    defaultLog.info(`${req.method} ${req.url}`);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization,responseType');
+    res.setHeader('Access-Control-Expose-Headers', 'x-total-count,x-pending-comment-count,x-next-comment-id');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Cache-Control', 'max-age=4');
+    next();
 });
 
-// Dynamically set the hostname based on what environment we're in.
-swaggerConfig.host = hostname;
-
-// Swagger UI needs to be told that we only serve https in Openshift
-if (hostname !== 'localhost:3000') {
-  swaggerConfig.schemes = ['https'];
-}
-
-swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
-  app.use(middleware.swaggerMetadata());
-
-  // TODO: Fix this
-  // app.use(middleware.swaggerValidator({ validateResponse: false}));
-
-  app.use(
-    middleware.swaggerSecurity({
-      Bearer: auth.verifyToken
-    })
-  );
-  
-  var routerConfig = {
-    controllers: "./api/controllers",
-    useStubs: false
-  };
-
-  app.use(middleware.swaggerRouter(routerConfig));
-
-  app.use(middleware.swaggerUi({apiDocs: '/api/docs', swaggerUi: '/api/docs'}));
-
-  // Make sure uploads directory exists
-  try {
-    if (!fs.existsSync(uploadDir)){
-        fs.mkdirSync(uploadDir);
-    }
-  } catch (e) {
-    // Fall through - uploads will continue to fail until this is resolved locally.
-    defaultLog.info("Couldn't create upload folder:", e);
-  }
-  // Load up DB
-  var options = {
-    useMongoClient: true,
-    poolSize: 10,
-    user: db_username,
-    pass: db_password,
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
-    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-    socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
-  };
-  defaultLog.info("Connecting to:", dbConnection);
-  mongoose.Promise  = global.Promise;
-  var db = mongoose.connect(dbConnection, options).then(
-    () => {
-      defaultLog.info("Database connected");
-
-      // Load database models
-      defaultLog.info("loading db models.");
-      require('./api/helpers/models/audit');
-      require('./api/helpers/models/list');
-      require('./api/helpers/models/user');
-      require('./api/helpers/models/group');
-      require('./api/helpers/models/pin');
-      require('./api/helpers/models/organization');
-      require('./api/helpers/models/vc');
-      require('./api/helpers/models/inspection');
-      require('./api/helpers/models/project');
-      require('./api/helpers/models/recentActivity');
-      require('./api/helpers/models/document');
-      require('./api/helpers/models/comment');
-      require('./api/helpers/models/commentperiod');
-      require('./api/helpers/models/topic');
-      defaultLog.info("db model loading done.");
-
-      app.listen(3000, '0.0.0.0', function() {
-        defaultLog.info("Started server on port 3000");
-      });
-    },
-    err => {
-      defaultLog.info("err:", err);
-      return;
-    });
-});
+export default app;
